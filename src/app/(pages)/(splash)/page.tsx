7dd941bced0c5886a56b1dde7333bd5086527d79
gyull <kuri6233@gmail.com>
2024-08-06 17:10:04 +0900

    [FEAT] splash 페이지 리디렉트 기능 추가

--- src/app/(pages)/(splash)/page.tsx
+++ src/app/(pages)/(splash)/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const SplashPage = () => {
+  const router = useRouter();
+
+  // 3초 후에 /login 페이지로 리디렉션
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/login');
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-end min-h-screen h-full px-12 py-14 bg-[#FFF9EA]">
       <div className="w-full flex-grow flex flex-col justify-center items-center gap-4">
