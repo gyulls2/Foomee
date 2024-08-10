71bcabae3bb24b0e15aeb5129783bf84382aa9ff
gyull <kuri6233@gmail.com>
2024-08-10 16:12:21 +0900

    [FEAT] 음식 검색 기능 추가 #17
    - 식약처 api 연동
    - 음식 검색 기능 구현
    - 음식 검색 결과를 리스트로 출력

---
+++ src/app/(pages)/search/FoodCard.tsx
@@ -0,0 +1,29 @@
+import { AddIcon } from '@/components/icons/IconComponents';
+
+type Props = {};
+
+const FoodCard = ({ item }) => {
+  console.log(item);
+  return (
+    <div className="flex items-center justify-between px-8 py-6 bg-white rounded-2xl mb-4">
+      <div>
+        <span className="text-lg font-semibold">
+          {item.FOOD_NM_KR.replaceAll('_', ' ')}
+        </span>
+        <p className="text-sm text-gray-500 pt-1">
+          1인분 ({item.SERVING_SIZE})
+        </p>
+      </div>
+      <div className="flex flex-col items-end gap-2">
+        <button className="w-8 h-8 flex items-center justify-center bg-[#FFB800] rounded-full">
+          <AddIcon fill="#ffffff" />
+        </button>
+        <span className="text-sm font-medium text-gray-500">
+          {item.AMT_NUM1}kcal
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,16 +1,83 @@
+'use client';
+
 import AddFoodSheet from '@/components/layout/AddFoodSheet';
 import {
   BackArrowIcon,
   SearchIcon,
-  SmileyIcon,
-  AddIcon,
+  MealIcon,
 } from '@/components/icons/IconComponents';
 import BottomNav from '@/components/layout/BottomNav';
+import { useForm } from 'react-hook-form';
+import { useEffect, useRef, useState } from 'react';
+import _ from 'lodash';
+import FoodCard from './FoodCard';
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+async function fetchData(foodName: string) {
+  console.log('fetchData 실행 : ', foodName);
+  try {
+    const url =
+      'https://apis.data.go.kr/1471000/FoodNtrCpntDbInfo01/getFoodNtrCpntDbInq01';
+    const queryParams =
+      '?' +
+      encodeURIComponent('serviceKey') +
+      '=' +
+      encodeURIComponent(API_KEY ?? '') +
+      '&' +
+      encodeURIComponent('pageNo') +
+      '=' +
+      encodeURIComponent('1') +
+      '&' +
+      encodeURIComponent('numOfRows') +
+      '=' +
+      encodeURIComponent('10') +
+      '&' +
+      encodeURIComponent('type') +
+      '=' +
+      encodeURIComponent('json') +
+      '&' +
+      encodeURIComponent('FOOD_NM_KR') +
+      '=' +
+      encodeURIComponent(foodName);
+
+    const response = await fetch(url + queryParams);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const resJson = await response.json();
+    return resJson.body;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
 
 const SearchPage = () => {
+  const { register, watch } = useForm();
+  const [foodList, setFoodList] = useState([]);
+
+  const inputValue = watch('foodName');
+
+  const debouncedData = useRef(
+    _.debounce(async (foodName: string) => {
+      const data = await fetchData(foodName);
+      console.log('debouncedData 실행 : ', data);
+      if (data) {
+        setFoodList(data.items);
+      }
+    }, 1000),
+  ).current;
+
+  useEffect(() => {
+    if (inputValue) {
+      debouncedData(inputValue);
+      console.log('useEffect 실행 : ', foodList);
+    }
+  }, [inputValue, debouncedData]);
+
   return (
     <main className="flex-col justify-center min-h-screen h-full bg-white">
-      <AddFoodSheet />
+      {/* <AddFoodSheet /> */}
 
       <header className="flex text-center relative w-full px-4 py-4 gap-3 items-center">
         <button>
@@ -20,6 +87,7 @@ const SearchPage = () => {
           <input
             className="rounded-full w-full h-10 bg-[#F5F5F5] pl-12 pr-4 focus:border-orange-400 focus:outline-none"
             placeholder="무슨 음식을 드셨나요?"
+            {...register('foodName', { required: true })}
           />
           <span className="absolute bottom-2 left-4">
             <SearchIcon />
@@ -29,26 +97,21 @@ const SearchPage = () => {
           <span className="font-bold text-gray-400">0</span>
         </div>
       </header>
-      <section className="px-8 flex flex-col gap-8 relative w-full h-full min-h-without-search-tab bg-[#FFFBF1] items-center py-4">
-        {/* <div className="flex flex-col gap-8 items-center my-auto">
-          <SmileyIcon />
-          <h2>오늘 먹은 음식을 검색해보세요</h2>
-        </div> */}
-
-        <div className="w-full">
-          <div className="flex items-center justify-between px-8 py-6 bg-white rounded-2xl">
-            <div>
-              <span className="text-lg font-semibold">된장찌개</span>
-              <p className="text-sm text-gray-500 pt-1">1인분 (200g)</p>
-            </div>
-            <div className="flex flex-col items-end gap-2">
-              <button className="w-8 h-8 flex items-center justify-center bg-[#FFB800] rounded-full">
-                <AddIcon fill="#ffffff" />
-              </button>
-              <span className="text-sm font-medium text-gray-500">92kcal</span>
-            </div>
+      <section className="px-8 flex flex-col gap-8 relative w-full h-full min-h-without-search-tab bg-[#FFFBF1] items-center pt-4 pb-20">
+        {!foodList.length && (
+          <div className="flex flex-col gap-4 items-center my-auto">
+            <MealIcon height="52" width="52" />
+            <h2>오늘 먹은 음식을 검색해보세요</h2>
           </div>
-        </div>
+        )}
+
+        {foodList.length > 0 && (
+          <div className="w-full">
+            {foodList.map(item => (
+              <FoodCard key={item.FOOD_CD} item={item} />
+            ))}
+          </div>
+        )}
       </section>
 
       <BottomNav />
