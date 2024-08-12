e5abca203c04a88287b70cfad0d8cb28a9c1c590
gyull <kuri6233@gmail.com>
2024-08-13 07:34:34 +0900

    [DESIGN] 캘린더 페이지 퍼블리싱 작업중 #34

--- src/app/(pages)/home/calendar/CalendarSeciont.tsx
+++ src/app/(pages)/home/calendar/CalendarSeciont.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { DropDownIcon } from '@/components/icons/IconComponents';
 import moment from 'moment';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
@@ -14,29 +13,30 @@ const CalendarSeciont = () => {
   const [value, onChange] = useState<Value>(new Date());
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex items-center">
-        <h1 className="font-semibold leading-9 text-2xl text-gray-900">
-          2024.07
-        </h1>
-        <DropDownIcon width="32" height="32" />
-      </div>
-      <div className="flex gap-2">
-        <button className="bg-main-primary-yellow py-2 px-3 rounded-full flex items-center">
-          <div className="rounded-full w-4 h-4 bg-point-green mr-2"></div>
+    <div className="flex flex-col gap-4 relative">
+      <div className="flex gap-2 absolute top-16">
+        <button className="bg-main-primary-yellow py-1 px-2 rounded-full flex items-center">
+          <div className="rounded-full w-3 h-3 bg-point-green mr-2"></div>
           <p className="text-white text-sm">먹었어요</p>
         </button>
-        <button className="border-2 border-main-primary-yellow py-2 px-3 rounded-full flex items-center">
-          <div className="rounded-full w-4 h-4 bg-point-pink mr-2"></div>
+        <button className="border-2 border-main-primary-yellow py-1 px-2 rounded-full flex items-center">
+          <div className="rounded-full w-3 h-3 bg-point-pink mr-2"></div>
           <p className="text-sm">몸무게</p>
         </button>
       </div>
 
-      <div>
+      <div className="mt-2">
         <Calendar
           onChange={onChange}
           value={value}
+          next2Label={null}
+          prev2Label={null}
+          showNeighboringMonth={false}
           formatDay={(locale, date) => moment(date).format('D')}
+          formatYear={(locale, date) => moment(date).format('YYYY')}
+          formatMonthYear={(locale, date) => moment(date).format('YYYY. MM')}
+          minDetail="year"
+          calendarType="gregory"
         />
       </div>
     </div>
--- src/app/(pages)/home/calendar/page.tsx
+++ src/app/(pages)/home/calendar/page.tsx
@@ -12,7 +12,12 @@ const CalendarPage = () => {
       </header>
       <section className="py-2.5 px-8 flex flex-col gap-8 relative w-full h-full min-h-without-header">
         <CalendarSeciont />
-        <div className="mt-auto">
+        <div className="mt-auto flex gap-4">
+          <button className="rounded-full w-6/12 h-14 border-2 border-main-primary-yellow">
+            <p className="text-center font-semibold leading-5 text-lg text-main-primary-yellow">
+              오늘로 이동
+            </p>
+          </button>
           <button className="rounded-full w-full h-14 bg-[#ffb800]">
             <p className="text-center font-semibold leading-5 text-lg text-neutral-100">
               선택한 날짜로 이동
