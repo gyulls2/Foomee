1b03164ba1ba67667723b2c50c2ec73b4d43a760
gyulls2 <kuri6233@gmail.com>
2024-08-21 12:20:00 +0900

    [FIX] 회원가입 환영 모달 오류 해결 #62

--- src/app/(pages)/(user)/signup/(goal)/step1/page.tsx
+++ src/app/(pages)/(user)/signup/(goal)/step1/page.tsx
@@ -2,7 +2,9 @@
 
 import { SmileyIcon } from '@/components/icons/IconComponents';
 import StepIndicator from '@/components/StepIndicator';
+import WelcomeModal from '@/components/WelcomeModal';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 const Step1Page = () => {
@@ -15,6 +17,7 @@ const Step1Page = () => {
   const selectedGender = watch('gender');
   const selectedActive = watch('activity-level');
   const router = useRouter();
+  const [isOpened, setIsOpened] = useState(true);
 
   const handleNext = async () => {
     const isValid = await trigger([
@@ -29,233 +32,236 @@ const Step1Page = () => {
   };
 
   return (
-    <div className="flex flex-col gap-6 min-h-full h-full">
-      <StepIndicator current="1" />
-      <h2 className="font-semibold leading-9 text-2xl text-gray-900">
-        맞춤 목표 계산 시작!
-        <br />
-        기본 정보를 알려주세요
-      </h2>
-      <div className="flex flex-col items-start">
-        <label
-          className="text-center leading-7 text-sm text-[#757575] mb-2"
-          htmlFor="gender"
-        >
-          성별
-        </label>
-        <div className="flex justify-center space-x-8 self-center">
-          <label className="flex flex-col items-center cursor-pointer">
-            <input
-              type="radio"
-              value="female"
-              className="hidden"
-              {...register('gender', { required: '성별은 필수입니다.' })}
-            />
-            <div
-              className={`w-16 h-16 rounded-full flex items-center justify-center ${
-                selectedGender === 'female'
-                  ? 'bg-main-secondary-yellow'
-                  : 'bg-gray-200'
-              }`}
-            >
-              <SmileyIcon />
-            </div>
-            <span className="text-gray-500 mt-4">여성</span>
-          </label>
-          <label className="flex flex-col items-center cursor-pointer">
-            <input
-              type="radio"
-              value="male"
-              className="hidden"
-              {...register('gender', { required: '성별은 필수입니다.' })}
-            />
-            <div
-              className={`w-16 h-16 rounded-full flex items-center justify-center ${
-                selectedGender === 'male'
-                  ? 'bg-main-secondary-yellow'
-                  : 'bg-gray-200'
-              }`}
-            >
-              <SmileyIcon />
-            </div>
-            <span className="text-gray-500 mt-4">남성</span>
-          </label>
-        </div>
-      </div>
-      <div className="flex gap-4">
-        <div>
+    <>
+      {isOpened && <WelcomeModal setIsOpened={setIsOpened} />}
+      <div className="flex flex-col gap-6 min-h-full h-full">
+        <StepIndicator current="1" />
+        <h2 className="font-semibold leading-9 text-2xl text-gray-900">
+          맞춤 목표 계산 시작!
+          <br />
+          기본 정보를 알려주세요
+        </h2>
+        <div className="flex flex-col items-start">
           <label
-            className="text-center leading-7 text-sm text-[#757575]"
-            htmlFor="age"
+            className="text-center leading-7 text-sm text-[#757575] mb-2"
+            htmlFor="gender"
           >
-            나이
+            성별
           </label>
-          <div className="relative mt-2">
-            <input
-              id="age"
-              type="number"
-              placeholder="0"
-              className="rounded-lg w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6 font-semibold pr-14"
-              {...register('age', {
-                required: '나이는 필수입니다.',
-                min: 1,
-                max: 100,
-              })}
-            />
-            <span className="absolute bottom-4 right-6 text-gray-400 font-semibold">
-              세
-            </span>
+          <div className="flex justify-center space-x-8 self-center">
+            <label className="flex flex-col items-center cursor-pointer">
+              <input
+                type="radio"
+                value="female"
+                className="hidden"
+                {...register('gender', { required: '성별은 필수입니다.' })}
+              />
+              <div
+                className={`w-16 h-16 rounded-full flex items-center justify-center ${
+                  selectedGender === 'female'
+                    ? 'bg-main-secondary-yellow'
+                    : 'bg-gray-200'
+                }`}
+              >
+                <SmileyIcon />
+              </div>
+              <span className="text-gray-500 mt-4">여성</span>
+            </label>
+            <label className="flex flex-col items-center cursor-pointer">
+              <input
+                type="radio"
+                value="male"
+                className="hidden"
+                {...register('gender', { required: '성별은 필수입니다.' })}
+              />
+              <div
+                className={`w-16 h-16 rounded-full flex items-center justify-center ${
+                  selectedGender === 'male'
+                    ? 'bg-main-secondary-yellow'
+                    : 'bg-gray-200'
+                }`}
+              >
+                <SmileyIcon />
+              </div>
+              <span className="text-gray-500 mt-4">남성</span>
+            </label>
           </div>
         </div>
-        <div>
-          <label
-            className="text-center leading-7 text-sm text-[#757575]"
-            htmlFor="height"
-          >
-            키
-          </label>
-          <div className="relative mt-2">
-            <input
-              id="height"
-              type="number"
-              placeholder="0"
-              className="rounded-lg w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6 font-semibold pr-14"
-              {...register('height', {
-                required: '키는 필수입니다.',
-                min: 1,
-                max: 200,
-              })}
-            />
-            <span className="absolute bottom-4 right-6 text-gray-400 font-semibold">
-              cm
-            </span>
+        <div className="flex gap-4">
+          <div>
+            <label
+              className="text-center leading-7 text-sm text-[#757575]"
+              htmlFor="age"
+            >
+              나이
+            </label>
+            <div className="relative mt-2">
+              <input
+                id="age"
+                type="number"
+                placeholder="0"
+                className="rounded-lg w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6 font-semibold pr-14"
+                {...register('age', {
+                  required: '나이는 필수입니다.',
+                  min: 1,
+                  max: 100,
+                })}
+              />
+              <span className="absolute bottom-4 right-6 text-gray-400 font-semibold">
+                세
+              </span>
+            </div>
+          </div>
+          <div>
+            <label
+              className="text-center leading-7 text-sm text-[#757575]"
+              htmlFor="height"
+            >
+              키
+            </label>
+            <div className="relative mt-2">
+              <input
+                id="height"
+                type="number"
+                placeholder="0"
+                className="rounded-lg w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6 font-semibold pr-14"
+                {...register('height', {
+                  required: '키는 필수입니다.',
+                  min: 1,
+                  max: 200,
+                })}
+              />
+              <span className="absolute bottom-4 right-6 text-gray-400 font-semibold">
+                cm
+              </span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex flex-col items-start">
-        <label
-          className="text-center leading-7 text-sm text-[#757575] mb-4"
-          htmlFor="activity-level"
-        >
-          평소 활동량
-        </label>
-        <div className="flex justify-center self-center space-x-2">
-          <label className="flex flex-col items-center cursor-pointer">
-            <input
-              type="radio"
-              value={1}
-              className="hidden"
-              {...register('activity-level', {
-                required: '활동량은 필수입니다.',
-              })}
-            />
-            <div
-              className={`w-16 h-16 rounded-full flex items-center justify-center ${
-                selectedActive === '1'
-                  ? 'bg-main-secondary-yellow'
-                  : 'bg-gray-200'
-              }`}
-            >
-              <SmileyIcon />
-            </div>
-            <span className="text-gray-500 mt-4 mb-2">매우 적음</span>
-            <p className="text-xs text-gray-500 text-center">
-              활동이 거의 없는
-              <br />
-              집돌이 집순이
-            </p>
+        <div className="flex flex-col items-start">
+          <label
+            className="text-center leading-7 text-sm text-[#757575] mb-4"
+            htmlFor="activity-level"
+          >
+            평소 활동량
           </label>
-          <label className="flex flex-col items-center cursor-pointer">
-            <input
-              type="radio"
-              value={2}
-              className="hidden"
-              {...register('activity-level', {
-                required: '활동량은 필수입니다.',
-              })}
-            />
-            <div
-              className={`w-16 h-16 rounded-full flex items-center justify-center ${
-                selectedActive === '2'
-                  ? 'bg-main-secondary-yellow'
-                  : 'bg-gray-200'
-              }`}
-            >
-              <SmileyIcon />
-            </div>
-            <span className="text-gray-500 mt-4 mb-2">적음</span>
-            <p className="text-xs text-gray-500 text-center">
-              주로 앉아있는
-              <br />
-              학생이나 직장인
+          <div className="flex justify-center self-center space-x-2">
+            <label className="flex flex-col items-center cursor-pointer">
+              <input
+                type="radio"
+                value={1}
+                className="hidden"
+                {...register('activity-level', {
+                  required: '활동량은 필수입니다.',
+                })}
+              />
+              <div
+                className={`w-16 h-16 rounded-full flex items-center justify-center ${
+                  selectedActive === '1'
+                    ? 'bg-main-secondary-yellow'
+                    : 'bg-gray-200'
+                }`}
+              >
+                <SmileyIcon />
+              </div>
+              <span className="text-gray-500 mt-4 mb-2">매우 적음</span>
+              <p className="text-xs text-gray-500 text-center">
+                활동이 거의 없는
+                <br />
+                집돌이 집순이
+              </p>
+            </label>
+            <label className="flex flex-col items-center cursor-pointer">
+              <input
+                type="radio"
+                value={2}
+                className="hidden"
+                {...register('activity-level', {
+                  required: '활동량은 필수입니다.',
+                })}
+              />
+              <div
+                className={`w-16 h-16 rounded-full flex items-center justify-center ${
+                  selectedActive === '2'
+                    ? 'bg-main-secondary-yellow'
+                    : 'bg-gray-200'
+                }`}
+              >
+                <SmileyIcon />
+              </div>
+              <span className="text-gray-500 mt-4 mb-2">적음</span>
+              <p className="text-xs text-gray-500 text-center">
+                주로 앉아있는
+                <br />
+                학생이나 직장인
+              </p>
+            </label>
+            <label className="flex flex-col items-center cursor-pointer">
+              <input
+                type="radio"
+                value={3}
+                className="hidden"
+                {...register('activity-level', {
+                  required: '활동량은 필수입니다.',
+                })}
+              />
+              <div
+                className={`w-16 h-16 rounded-full flex items-center justify-center ${
+                  selectedActive === '3'
+                    ? 'bg-main-secondary-yellow'
+                    : 'bg-gray-200'
+                }`}
+              >
+                <SmileyIcon />
+              </div>
+              <span className="text-gray-500 mt-4 mb-2">보통</span>
+              <p className="text-xs text-gray-500 text-center">
+                주 2~3회 정도
+                <br />
+                운동하는 꾸준러
+              </p>
+            </label>
+            <label className="flex flex-col items-center cursor-pointer">
+              <input
+                type="radio"
+                value={4}
+                className="hidden"
+                {...register('activity-level', {
+                  required: '활동량은 필수입니다.',
+                })}
+              />
+              <div
+                className={`w-16 h-16 rounded-full flex items-center justify-center ${
+                  selectedActive === '4'
+                    ? 'bg-main-secondary-yellow'
+                    : 'bg-gray-200'
+                }`}
+              >
+                <SmileyIcon />
+              </div>
+              <span className="text-gray-500 mt-4 mb-2">많음</span>
+              <p className="text-xs text-gray-500 text-center">
+                매일 뛰거나
+                <br />
+                운동하는 갓생러
+              </p>
+            </label>
+          </div>
+        </div>
+
+        <div className="mt-auto">
+          <button
+            className="rounded-full w-full h-14 bg-[#ffb800] disabled:opacity-50"
+            disabled={!isValid}
+            onClick={handleNext}
+          >
+            <p className="text-center font-semibold leading-5 text-lg text-neutral-100">
+              다음
             </p>
-          </label>
-          <label className="flex flex-col items-center cursor-pointer">
-            <input
-              type="radio"
-              value={3}
-              className="hidden"
-              {...register('activity-level', {
-                required: '활동량은 필수입니다.',
-              })}
-            />
-            <div
-              className={`w-16 h-16 rounded-full flex items-center justify-center ${
-                selectedActive === '3'
-                  ? 'bg-main-secondary-yellow'
-                  : 'bg-gray-200'
-              }`}
-            >
-              <SmileyIcon />
-            </div>
-            <span className="text-gray-500 mt-4 mb-2">보통</span>
-            <p className="text-xs text-gray-500 text-center">
-              주 2~3회 정도
-              <br />
-              운동하는 꾸준러
-            </p>
-          </label>
-          <label className="flex flex-col items-center cursor-pointer">
-            <input
-              type="radio"
-              value={4}
-              className="hidden"
-              {...register('activity-level', {
-                required: '활동량은 필수입니다.',
-              })}
-            />
-            <div
-              className={`w-16 h-16 rounded-full flex items-center justify-center ${
-                selectedActive === '4'
-                  ? 'bg-main-secondary-yellow'
-                  : 'bg-gray-200'
-              }`}
-            >
-              <SmileyIcon />
-            </div>
-            <span className="text-gray-500 mt-4 mb-2">많음</span>
-            <p className="text-xs text-gray-500 text-center">
-              매일 뛰거나
-              <br />
-              운동하는 갓생러
-            </p>
-          </label>
+          </button>
         </div>
       </div>
-
-      <div className="mt-auto">
-        <button
-          className="rounded-full w-full h-14 bg-[#ffb800] disabled:opacity-50"
-          disabled={!isValid}
-          onClick={handleNext}
-        >
-          <p className="text-center font-semibold leading-5 text-lg text-neutral-100">
-            다음
-          </p>
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/app/(pages)/(user)/signup/SignupForm.tsx
+++ src/app/(pages)/(user)/signup/SignupForm.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import InputError from '@/components/InputError';
-import WelcomeModal from '@/components/WelcomeModal';
 import { signInWithCredentials } from '@/data/actions/authAction';
 import { signup } from '@/data/actions/userAction';
 import { UserForm } from '@/types';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
 const SignupForm = () => {
@@ -14,7 +13,7 @@ const SignupForm = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<UserForm>();
-  const [isOpened, setIsOpened] = useState(false);
+  const router = useRouter();
 
   const onSubmit = async (data: UserForm) => {
     data.type = 'user';
@@ -26,71 +25,68 @@ const SignupForm = () => {
         email: data.email,
         password: data.password,
       });
-      if (isLogin) setIsOpened(true);
+      if (isLogin) router.push('/signup/step1');
     }
   };
 
   return (
-    <>
-      {isOpened && <WelcomeModal setIsOpened={setIsOpened} />}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-3.5 items-start w-full"
-      >
-        <label htmlFor="name" className="sr-only">
-          이름
-        </label>
-        <input
-          id="name"
-          placeholder="이름을 입력하세요"
-          className="rounded-full w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6"
-          {...register('name', { required: '이름은 필수입니다.' })}
-        />
-        <InputError target={errors.name} />
-        <label htmlFor="email" className="sr-only">
-          이메일
-        </label>
-        <input
-          id="email"
-          type="email"
-          placeholder="이메일을 입력하세요"
-          className="rounded-full w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6"
-          {...register('email', {
-            required: '이메일은 필수입니다.',
-            pattern: {
-              value:
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
-              message: '형식에 맞지 않는 이메일입니다.',
-            },
-          })}
-        />
-        <InputError target={errors.email} />
-        <label htmlFor="password" className="sr-only">
-          비밀번호
-        </label>
-        <input
-          id="password"
-          type="password"
-          placeholder="비밀번호를 입력하세요"
-          className="rounded-full w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6"
-          {...register('password', {
-            required: '비밀번호는 필수입니다.',
-            minLength: {
-              value: 8,
-              message: '비밀번호는 최소 8자 이상이어야 합니다.',
-            },
-          })}
-        />
-        <InputError target={errors.password} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-3.5 items-start w-full"
+    >
+      <label htmlFor="name" className="sr-only">
+        이름
+      </label>
+      <input
+        id="name"
+        placeholder="이름을 입력하세요"
+        className="rounded-full w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6"
+        {...register('name', { required: '이름은 필수입니다.' })}
+      />
+      <InputError target={errors.name} />
+      <label htmlFor="email" className="sr-only">
+        이메일
+      </label>
+      <input
+        id="email"
+        type="email"
+        placeholder="이메일을 입력하세요"
+        className="rounded-full w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6"
+        {...register('email', {
+          required: '이메일은 필수입니다.',
+          pattern: {
+            value:
+              /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+            message: '형식에 맞지 않는 이메일입니다.',
+          },
+        })}
+      />
+      <InputError target={errors.email} />
+      <label htmlFor="password" className="sr-only">
+        비밀번호
+      </label>
+      <input
+        id="password"
+        type="password"
+        placeholder="비밀번호를 입력하세요"
+        className="rounded-full w-full h-14 bg-[#fff7e1] focus:outline-none focus:border-orange-400 px-6"
+        {...register('password', {
+          required: '비밀번호는 필수입니다.',
+          minLength: {
+            value: 8,
+            message: '비밀번호는 최소 8자 이상이어야 합니다.',
+          },
+        })}
+      />
+      <InputError target={errors.password} />
 
-        <button
-          type="submit"
-          className="rounded-full w-full h-14 bg-[#ffb800] text-center font-semibold leading-7 text-lg text-neutral-100"
-        >
-          회원가입
-        </button>
-      </form>
-    </>
+      <button
+        type="submit"
+        className="rounded-full w-full h-14 bg-[#ffb800] text-center font-semibold leading-7 text-lg text-neutral-100"
+      >
+        회원가입
+      </button>
+    </form>
   );
 };
 
