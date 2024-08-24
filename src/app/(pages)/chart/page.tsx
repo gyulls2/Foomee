2e1b7ff52e207028b31ade2c66f87670c472f148
gyulls2 <kuri6233@gmail.com>
2024-08-24 21:13:33 +0900

    [FEAT] 통계 페이지 체중 목표, 섭취량 조회 #75
    - 유저 데이터 - 시작 체중, 목표 체중 조회
    - weight 게시판 - 가장 최근 입력한 체중 조회, 시작 체중과 차이 계산
    - nutri 게시판 - 오늘 섭취량 조회

--- src/app/(pages)/chart/page.tsx
+++ src/app/(pages)/chart/page.tsx
@@ -1,15 +1,75 @@
 import BottomNav from '@/components/layout/BottomNav';
 import Slider from './Slider';
+import { auth } from '@/auth';
+import { UserData } from '@/types';
+import { fetchUser } from '@/data/fetch/userFetch';
+import moment from 'moment';
+import { fetchPosts } from '@/data/fetch/postFetch';
 
-const ChartPage = () => {
+const ChartPage = async () => {
+  const session = await auth();
+
+  // 사용자 extra 정보 조회
+  const fetchUserData = async (): Promise<UserData | undefined> => {
+    if (!session?.user) {
+      throw new Error('User is not authenticated');
+    }
+    try {
+      const userData = await fetchUser(session.user._id, session.accessToken);
+      if (!userData) {
+        throw new Error('Failed to fetch user data');
+      }
+      return userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+  const user = await fetchUserData();
+
+  // 섭취 칼로리 데이터 불러오기
+  const fetchCalorie = async () => {
+    const keyword = moment(new Date()).format('YYYY.MM.DD');
+    const response = await fetchPosts('nutri', undefined, keyword);
+    if (response.length > 0) {
+      return response[0].extra?.enerc;
+    } else {
+      return 0;
+    }
+  };
+  const enerc = await fetchCalorie();
+
+  // 가장 최근 체중 데이터 불러오기
+  const fetchWeight = async () => {
+    const response = await fetchPosts('weight');
+    if (response.length > 0) {
+      const mostRecent = response.reduce((latest, current) => {
+        const latestDate = moment(latest.title, 'MMMM.YY.DD');
+        const currentDate = moment(current.title, 'MMMM.YY.DD');
+
+        return currentDate.isAfter(latestDate) ? current : latest;
+      });
+      return parseFloat(mostRecent.content);
+    } else {
+      return 0;
+    }
+  };
+  const weight = await fetchWeight();
+
+  const weightDiff = (user?.extra?.starting_weight ?? 0) - weight;
+  const formattedWeightDiff =
+    weightDiff > 0 ? `-${weightDiff}` : `+${Math.abs(weightDiff)}`;
+
   return (
     <main className="flex-col justify-center min-h-screen h-full ">
       <section className="pb-20 px-8 flex flex-col gap-6 relative w-full pt-8 h-full min-h-without-tab bg-bg-light-yellow justify-center">
         <div>
           <h2 className="text-lg font-semibold">
-            오늘 하루 2000kcal 먹었어요.
+            오늘 하루 {enerc}kcal 먹었어요.
           </h2>
-          <p className="mt-2">⛳ 목표 50kg 😊 지금까지 -5kg</p>
+          <p className="mt-2">
+            ⛳ 목표 {user?.extra?.goal_weight}kg 😊 지금까지
+            {formattedWeightDiff}kg
+          </p>
         </div>
         <Slider />
       </section>
